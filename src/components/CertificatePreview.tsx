d64ff1c8d59122d29a8a6e20a05a4de68c5b89cc
Ndanyuzwe Duncan <semunda9@gmail.com>
2025-06-13 17:01:00 +0200

    done

--- src/components/CertificatePreview.tsx
+++ src/components/CertificatePreview.tsx
@@ -48,7 +48,7 @@ export default function CertificatePreview({ name, year, certNumber, imagePrevie
       </div> */}
       <p className=" absolute top-[460px]  left-[400px] right-[400px] text-center  text-black  text-sm" >is a registered and licensed</p>
       <p className="absolute top-[480px] left-[210px] text-sm text-black">
-        <span className="font-bold">Architect in the year of {year} as a Professional Chartered Member</span> of
+        <span className="font-bold">Architect in the year of {new Date().getFullYear()} as a professional senior corporate Member</span> of
         the Institute
         in accordance with <br />the terms of a complying member under the Architects, Landscape Arch itects, Urban Designers, and <br></br>
         <span className="text-center">Interior Designers registration and building codes within the laws of Rwanda.</span>
@@ -66,13 +66,13 @@ export default function CertificatePreview({ name, year, certNumber, imagePrevie
           className="absolute top-[470px] left-[100px] w-[100px] h-[100px] object-cover rounded"
         />
       )}
-      <span className="absolute top-[605px] left-[220px] text-sm text-black">A.134</span>
-      <span className="absolute top-[635px] left-[120px] text-sm text-black">
-        <img src="/sign_emm.png" alt="Signature Emmanuel" className="w-[150px] h-auto" />
+      <span className="absolute top-[605px] left-[220px] text-sm text-black">A.74.17</span>
+      <span className="absolute top-[635px] left-[75px] text-sm text-black">
+        <img src="/sign_emm.png" alt="Signature Emmanuel" className="w-[200px] h-auto" />
       </span>
       <span className="absolute top-[670px] left-[110px] text-xs text-black">Arch. Emmanuel Nyirinkindi</span>
       <span className="absolute top-[595px] left-[255px] text-sm text-black">
-        <img src="/bg_RIA.png" alt="Signature RIA" className="w-[140px] h-auto" />
+        <img src="/bg_RIA.png" alt="Signature RIA" className="w-[140px]  h-auto" />
       </span>
       <span className="absolute top-[600px] right-[400px] text-sm text-black">
         <img src="/senior.png" alt="Senior" className="w-[400px] h-auto" />
@@ -82,11 +82,11 @@ export default function CertificatePreview({ name, year, certNumber, imagePrevie
       </div>
       <span className="absolute top-[605px] right-[160px] text-md text-black">20/05/2025</span>
       <span className="absolute top-[635px] right-[115px] text-sm text-black">
-        <img src="/sign_eu.png" alt="Signature Eudes" className="w-[150px] h-auto" />
+        <img src="/sign_eu.png" alt="Signature Eudes" className="w-[200px] h-auto" />
       </span>
 
       <span className="absolute top-[670px] right-[140px] text-xs text-black">Arch. Eudes Kayumba</span>
-      <span className="absolute top-[530px] right-[40px] text-sm text-black">
+      <span className="absolute top-[530px] right-[25px] text-sm text-black">
         <img src="/bg_RB.png" alt="Signature Eudes" className="w-[230px] h-auto" />
       </span>
       <p className="absolute top-[720px] left-0 right-0 text-white text-center py-1 font-bold" style={{ fontSize: 10.5 }}>
